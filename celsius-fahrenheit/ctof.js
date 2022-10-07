4f90d0d6c936f415e23e7a6cdcaf25759199dbff
DragonRF <duonglong410@gmail.com>
2022-10-07 16:34:46 +0700

    chu vi & dien tich hinh tron

---
+++ celsius-fahrenheit/ctof.js
@@ -0,0 +1,7 @@
+bankinh = prompt("Nhập bán kính hình tròn");
+let radius = parseInt(bankinh);
+let chuvi = (bankinh * 2) * 3.14;
+let dientich = ((chuvi * 2) / 4) * 3.14;
+document.write("Chu vi hình tròn là: " + chuvi);
+document.write("<br/>");
+document.write("Diện tích hình tròn là: " + dientich);
