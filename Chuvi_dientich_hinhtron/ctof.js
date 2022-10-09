5554199e9e392d3199c1f6c2b26310b97f4317dc
DragonRF <duonglong410@gmail.com>
2022-10-09 18:30:15 +0700

    thang10

--- Chuvi_dientich_hinhtron/ctof.js
+++ Chuvi_dientich_hinhtron/ctof.js
@@ -1,6 +1,6 @@
 bankinh = prompt("Nhập bán kính hình tròn");
 let radius = parseInt(bankinh);
-let chuvi = (bankinh * 2) * 3.14;
+let chuvi = (radius * 2) * 3.14;
 let dientich = ((chuvi * 2) / 4) * 3.14;
 document.write("Chu vi hình tròn là: " + chuvi);
 document.write("<br/>");
