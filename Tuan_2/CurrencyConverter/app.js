65776e3aa26a97457b583c325a026f84ba4241d2
DragonRF <duonglong410@gmail.com>
2022-10-10 14:28:32 +0700

    CodeGym

--- Tuan_2/CurrencyConverter/app.js
+++ Tuan_2/CurrencyConverter/app.js
@@ -4,8 +4,8 @@ const api = "https://api.exchangerate-api.com/v4/latest/USD";
 // for selecting different controls
 let search = document.querySelector(".searchBox");
 let convert = document.querySelector(".convert");
-let fromCurrecy = document.querySelector(".from");
-let toCurrecy = document.querySelector(".to");
+let fromCurrency = document.querySelector(".from");
+let toCurrency = document.querySelector(".to");
 let finalValue = document.querySelector(".finalValue");
 let finalAmount = document.getElementById("finalAmount");
 let resultFrom;
@@ -13,12 +13,12 @@ let resultTo;
 let searchValue;
 
 // Event when currency is changed
-fromCurrecy.addEventListener('change', (event) => {
+fromCurrency.addEventListener('change', (event) => {
     resultFrom = `${event.target.value}`;
 });
 
 // Event when currency is changed
-toCurrecy.addEventListener('change', (event) => {
+toCurrency.addEventListener('change', (event) => {
     resultTo = `${event.target.value}`;
 });
 
