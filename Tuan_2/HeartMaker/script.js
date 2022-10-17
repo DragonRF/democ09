56353b13ae6d34d62b9e925ede30a8bc2e406a59
DragonRF <duonglong410@gmail.com>
2022-10-17 10:07:34 +0700

    CodeGym

--- Tuan_2/HeartMaker/script.js
+++ Tuan_2/HeartMaker/script.js
@@ -1,5 +1,5 @@
-for(let row=0; row<=5; row++){
-    for(let col=0; col<=6; col++){
+for(let row = 0; row <= 5; row++){
+    for(let col = 0; col <= 6; col++){
         if(( (col%3!==0) && (row === 0) ) || ((col%3===0) && (row === 1))){
             document.write("* &nbsp");
         }else if(row - col === 2 || row + col === 8){
---
+++ Tuan_3/Array/For..of/demo/script.js
@@ -0,0 +1,6 @@
+// Tạo ra một mảng
+let ngonNgu = ["JS", "HTML", "PHP", "Python"];
+// Sử dụng for of để lặp qua mảng
+for (let x of ngonNgu) {
+    console.log(x);
+}
