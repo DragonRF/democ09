e8b31f1c153aa272648d5634493ab7f9b575890f
DragonRF <duonglong410@gmail.com>
2022-10-18 15:01:29 +0700

    CodeGym

---
+++ Tuan_3/Array/advance practice 2/bai 3/script.js
@@ -0,0 +1,6 @@
+let myArray = ["Hôm nay là ngày 18/10.", 18, 10, 2022, 10, 22, 20];
+let myLength = myArray.length;
+console.log("Độ dài của mảng là: " + myLength);
+for (let number of myArray) {
+    console.log(number);
+}
