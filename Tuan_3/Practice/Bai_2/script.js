bb8f664e16c7732a3fdc20956caec18df75af6c9
DragonRF <duonglong410@gmail.com>
2022-10-19 10:46:06 +0700

    CodeGym

--- Tuan_3/Practice/Bai_2/script.js
+++ Tuan_3/Practice/Bai_2/script.js
@@ -1,5 +1,5 @@
 let mang = [];
-let n = +prompt("Nhập số phần tử của mảng")
+let n = +prompt("Nhập số phần tử của mảng") //Nhập số phần tử n vào mảng
 let tong = 0;
 let chia5du2 = 0
 for (let i = 0; i < n; i++){
@@ -7,13 +7,27 @@ for (let i = 0; i < n; i++){
     document.write(mang[i]+"<br>");
     tong += mang[i];
 }
-document.write("Tổng các phần tử là: " + tong+"<br>");
+document.write("Tổng các phần tử là: " + tong + "<br>"); // Hiển thị tổng các phần tử
 
 for (let i = 0; i < n; i++) {
-    if (mang[i] % 5 === 2) {
+    if (mang[i] % 5 === 2)
         chia5du2 += mang[i];
-    }
-    document.write(mang[i] + "<br>")
-}
-document.write("Tổng các phần tử chia 5 dư 2 là: " + chia5du2)
 
+}
+document.write("Tổng các phần tử chia 5 dư 2 là: " + chia5du2 + "<br>") // Hiển thị tổng các phần tử chia 5 dư 2
+
+let k = 0
+for (let i = 0; i < n; i++) {
+    if (mang[i] % 6 === 0) k++;
+}
+document.write('Số phần tử chia hết cho 6: ' + k + '<br>'); // Hiển thị các phần tử chia hết cho 6
+
+let tbcc4d3 = 0;
+let tongtbc = 0;
+for (let i = 0; i < n; i++) {
+    if (mang[i] > 10 && mang[i] % 4 === 3) {
+        tongtbc += mang[i];
+        tbcc4d3 = tongtbc / i;
+    }
+}
+document.write("TBC các số lớn hơn 10 và chia 4 dư 3 là " + tbcc4d3 + "<br>")
